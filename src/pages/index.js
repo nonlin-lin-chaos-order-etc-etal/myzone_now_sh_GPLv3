a245f57f129a62cbacd79014729fca165e033aa3
nonlin-lin-chaos-order-etc-etal <19966907+nonlin-lin-chaos-order-etc-etal@users.noreply.github.com>
2021-05-25 01:09:15 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,11 @@ function Index() {
       </Helmet>
       <h1>🐸🚀🌌+∞ the nile starts here!</h1>
       <h2>⊙🐸🚀🌌+∞¤💎🧙🐝💞🏡🌈🐁💚 et cetera et al 🠞 the nile</h2>
-     for this shepherd's ( vis vitalis ↝↠⇉ ±∞ ) outpost
       <p>(* <a href="https://myzone.now.sh/">this site's facade</a> *)</p>
       <p>(* <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3">this site's GPL3 source code repo at GitHub</a> *)</p>
       <p>(* <a href="https://vercel.com/linear-et-nonlinear-chaos-order-etc-etal/myzone">this site's now.sh dashboard at Vercel</a> *)</p>
       <br/>
-      <p>(* ДВИЖКИ МОИХ ПРОЗРАЧНЫХ И НЕПРОЗРАЧНЫХ АМБАРОВ: <a href="https://myresourcesfeedbacksloops.now.sh/">🐸🚀🌌+∞¤ my feedbacks loops</a> *)</p>
+      <p>(* ДВИЖКИ МОИХ ПРОЗРАЧНЫХ И НЕПРОЗРАЧНЫХ АМБАРОВ: <a href="https://myresourcesfeedbacksloops.now.sh/">🐸🚀🌌+∞¤ my feedbacks loops</a>: shepherd's ( vis vitalis ↝↠⇉ ±∞ ) outpost *)</p>
       <br/>
       <p>
         Made with help of{' '}

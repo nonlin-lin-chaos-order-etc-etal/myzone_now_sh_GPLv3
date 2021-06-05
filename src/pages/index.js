59cc85a6fb239c30113fbd4f2c05eb34f2dd850f
Someone roleplaying as: GIANT_WORLDKEEPER: ИНЕИСТЫЙЪЬ ВЕЛИКАНЪЬ ХРАНИМИРЪЬ <hypnosis.i2p@gmail.com>
2021-06-05 14:52:39 +0800

    sublime-text 4 syntax highlighting red lines fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,34 +32,29 @@ function Index() {
       <br/>
       {/*<p> <GiantWorldsKeeperBannerVV8 src={imageOfGiantWorldsKeeperBannerVV8} alt="GiantWorldsKeeperBannerVisVitalisPaintingWithDetailsAndAPortrait8"/> </p>
       <br/>*/}
-      <p>(* <a href="https://myzone.now.sh/">this site's facade</a> *)</p>
-      <p>(* <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3">this site's GPL3 source code repo at GitHub</a> *)</p>
-      <p>(* <a href="https://vercel.com/linear-et-nonlinear-chaos-order-etc-etal/myzone">this site's now.sh dashboard at Vercel</a> *)</p>
+      <p>(* <a href="https://myzone.now.sh/">this site&apos;s facade</a> *)</p>
+      <p>(* <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3">this site&apos;s GPL3 source code repo at GitHub</a> *)</p>
+      <p>(* <a href="https://vercel.com/linear-et-nonlinear-chaos-order-etc-etal/myzone">this site&apos;s now.sh dashboard at Vercel</a> *)</p>
       <br/>
       <p>(*
         ДВИЖКИ МОИХ ПРОЗРАЧНЫХ (* С ОТКРЫТЫМИ ЧЕРТЕЖАМИ МАШИН *) И НЕПРОЗРАЧНЫХ (* БЕЗ ОТКРЫТЫХ ЧЕРТЕЖЕЙ *) АМБАРОВ, ИЛИ ЛЯГУШКИНЫ МУХИ+¤¤¤¤¤¤¤¤:
-        <a href="https://myresourcesfeedbacksloops.now.sh/">🐸🚀🌌+∞¤ my feedbacks loops</a>: shepherd's ( vis vitalis ↝↠⇉ ±∞ ) outpost
+        <a href="https://myresourcesfeedbacksloops.now.sh/">🐸🚀🌌+∞¤ my feedbacks loops</a>: shepherd&apos;s ( vis vitalis ↝↠⇉ ±∞ ) outpost
       *)</p>
       <br/>
       <p>
         Made with help of{' '}
         a <a href="https://github.com/vercel/vercel/tree/main/examples/gatsby" target="_blank" rel="noreferrer noopener">Gatsby now.sh template</a>.
         Is an app with two directories,{' '}
-        <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3/tree/main/src"><code>/src</code></a>{' '}
-        for static content and{' '}
-        <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3/tree/main/api"><code>/api</code></a>{' '}
+        <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3/tree/main/src"><code>/src</code></a>{' '}for static content and{' '}<a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3/tree/main/api"><code>/api</code></a>{' '}
         which contains a serverless{' '} <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date"><code>api/date</code> for the Date API with Node.js (TypeScript)</a>.
-        As an example, the date according to Node.js (TypeScript) is: {date ? date : 'Loading date...'} | https://dou.ua/lenta/articles/gatsbyjs-guide/
+        <a href="/api/date"><code>api/date</code> for the Date API with Node.js (TypeScript)</a>. As an example, the date according to Node.js (TypeScript) is: {date ? date : 'Loading date...'} | https://dou.ua/lenta/articles/gatsbyjs-guide/
       </p>
       <hr/>
-      Copyright © 1974.....2021 Someone Role Playing As GIANT WORLDKEEPER ИНЕИСТЫЙЪЬ ВЕЛИКАНЪЬ ХРАНИМИРЪЬ. All Rights Reserved.
+      Copyright © {'1974'}........{'2021'} Someone Role Playing As GIANT WORLDKEEPER ИНЕИСТЫЙЪЬ ВЕЛИКАНЪЬ ХРАНИМИРЪЬ. All Rights Reserved.
       <hr/>
       <p>
         Hosted by{' '} <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">Vercel</a>{' '} et {' '}
-        <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3">GitHub</a>
-        {' · '} 
-        Powered by{' '} <a href="https://www.gatsbyjs.org/" target="_blank" rel="noreferrer noopener">Gatsby</a>
+        <a href="https://github.com/nonlin-lin-chaos-order-etc-etal/myzone_now_sh_GPLv3">GitHub</a>{' · '}Powered by{' '} <a href="https://www.gatsbyjs.org/" target="_blank" rel="noreferrer noopener">Gatsby</a>
       </p>
     </main>
   );
